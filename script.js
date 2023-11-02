590a4fec9313f4c71b58cc332ef428f9d33a0334
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-02 14:19:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,9 +17,10 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+  // Use the filter method to create an array containing only read books (readingStatus is true)
+  const readBooks = library.filter((book) => book.readingStatus === true);
+  // Return the count of read books
+  return readBooks.length;
 };
 
-// Do not change the code below
-
-alert(numberOfBooksRead());
+alert(numberOfBooksRead()); // This should alert 2
